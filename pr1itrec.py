4f57bcc4b9d2fbe26fef30987790f15617835111
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-11-17 11:16:33 +0200

    Update pr1itrec.py

--- pr1itrec.py
+++ pr1itrec.py
@@ -1,19 +1,19 @@
 print("Iterativ: ")
 def sumaiterativa1(n):
-    s = 0
+    s1 = 0
     for i in range(1, n*2*(n - 1)):
-        s += i
-    return s
+        s1 += i
+    return s1
 n1 = int(input("a) n1 = "))
 print("a) ", sumaiterativa1(n1))
 
 print("\n")
 
 def sumaiterativa2(n):
-    s = 0
+    s2 = 0
     for i in range(1, n/(((2*n)-1)*((2*n)+1))):
-        s += i
-    return s
+        s2 += i
+    return s2
 n2 = int(input("b) n2 = "))
 print("b) ", sumaiterativa2(n2))
 
